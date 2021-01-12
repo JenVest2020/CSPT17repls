336daf75fad5f6fdce220cba5857f8ff5aa89096
JenVest2020 <jenvest2019@gmail.com>
2021-01-11 23:09:24 -0600

    module1 Objective5 completed

---
+++ mod1Obj5.py
@@ -0,0 +1,16 @@
+"""
+1. Create a "numbers" list that contains two different integer values and two different floating point values.
+
+2. Create a "strings" list that contains three different strings.
+
+3. Print the 4th number of your numbers list and the 1st string of your strings list.
+
+4. Iterate through your strings list and print each string.
+"""
+
+numbers = [7, 9, 47.9, 46.9]
+strings = ["Jenni", "Loves", "Allen"]
+print(numbers[3])
+print(strings[0])
+for string in strings:
+  print(string)
