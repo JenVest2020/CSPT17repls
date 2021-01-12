defd11d5eabf2c0cca2fb9b772efeed31f141e53
JenVest2020 <jenvest2019@gmail.com>
2021-01-11 22:56:29 -0600

    module1 Objective4 completed

---
+++ mod1Obj4.py
@@ -0,0 +1,19 @@
+"""
+Modify the assignment statements below so that an integer is assigned to "my_int", a floating point is assigned to "my_float", and a string is assigned to "my_string".
+
+Then, use typecasting to print your integer as a floating point number. Use typecasting again to print your floating point number as an integer.
+"""
+
+# Modify the code below to fulfill the requirements outlined in the docstring.
+my_int = 7
+my_float = 9.75
+my_string = "Forever and Always"
+
+#typecasting
+my_int = int(7.0)
+my_float = float (9)
+
+# Practice typecasting as outlined above with these print statements.
+print(my_int)
+print(my_float)
+print(my_string)
