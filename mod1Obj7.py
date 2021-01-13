6f573836ddb50f2a1cf788b1969b54da9e4ef1d1
JenVest2020 <jenvest2019@gmail.com>
2021-01-12 22:53:16 -0600

    module 1 objective7 completed

---
+++ mod1Obj7.py
@@ -0,0 +1,11 @@
+"""
+1. Assign three different types of data to the three variables "a", "b", and "c".
+
+2. Use a format string to inject the data from your three variables into the string.
+"""
+# Modify the code below to meet the requirements above.
+a = "Love"
+b = 13
+c = 13.13
+
+print("My favorite reference on %s is found in 1 Corinthians chapter %d with both chapter and verse as %.2f" % (a, b, c))
