577e9ebaba028904eaa0f2ed284b2288dc2176ac
JenVest2020 <jenvest2019@gmail.com>
2021-01-12 20:27:36 -0600

    module 1 objective 6 completed

---
+++ mod1Obj6.py
@@ -0,0 +1,10 @@
+1. Assign two different types to the variables "a" and "b".
+
+2. Use basic operators to create a list that contains three instances of "a" and three instances of "b".
+"""
+# Modify the code below to meet the requirements outlined above
+a = "Jenni"
+b = "47.9"
+
+my_list = [a] * 3 + [b] * 3
+print(my_list)
